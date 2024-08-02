e836fa2bc7d30a8845fba2a468ebccc52d2e440f
MaxFrequency <max.roberts@hey.com>
2024-08-02 14:20:30 -0400

    Quartz sync: Aug 2, 2024, 2:20 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,6 +8,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={classNames(displayClass, "page-title")}>
+      <img src="/static/og-image.png" />
       <a href={baseDir}>{title}</a>
     </h1>
   )
